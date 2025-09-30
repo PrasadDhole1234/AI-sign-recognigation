73e6f96cbfd51509290d8c6c64236b81d621bc80
Prasad Dhole <109674053+PrasadDhole1234@users.noreply.github.com>
2025-09-30 17:57:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,11 +45,10 @@ while True:
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
 
-
-
     cv2.imshow("Image", img)
     key=cv2.waitKey(1)
     if key == ord("s"):
         counter += 1
         cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
         print(counter)
+
